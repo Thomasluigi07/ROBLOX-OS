32c2fac26505a17efd9e90d413a9e2640ad69afb
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2021-04-14 12:31:57 +1100

    Initial commit

---
+++ Program.vb
@@ -0,0 +1,15 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.ForegroundColor = ConsoleColor.Black
+        Console.BackgroundColor = ConsoleColor.White
+        Console.Title = "ROBLOX OS"
+        Console.Beep()
+        Console.WriteLine("ROBLOX OS")
+        Console.WriteLine("Copyright 2021 ROBLOX Corporation")
+        Console.CursorVisible = False
+
+
+    End Sub
+End Module
